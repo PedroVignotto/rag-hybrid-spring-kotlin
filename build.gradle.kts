cc7ad5e0a4163c00a838b94d3e026bb465cd5f97
PedroVignotto <pedro.vignotto@hotmail.com>
2025-09-02 18:16:19 -0300

    build: ktlint Gradle plugin for Kotlin style

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
 	kotlin("jvm") version "1.9.25"
 	kotlin("plugin.spring") version "1.9.25"
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.1"
 	id("org.springframework.boot") version "3.5.5"
 	id("io.spring.dependency-management") version "1.1.7"
 }
@@ -36,4 +37,10 @@ kotlin {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+ktlint {
+    filter {
+        exclude("**/build/**")
+    }
 }
