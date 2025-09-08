ebfac3a1397f8aad1ef14dcbbf02f9060ee67c8c
PedroVignotto <pedro.vignotto@hotmail.com>
2025-09-07 21:25:40 -0300

    feat: add Ollama stream response model

---
+++ src/main/kotlin/dev/pedro/rag/infra/llm/ollama/model/response/OllamaChatStreamChunkResponse.kt
@@ -0,0 +1,19 @@
+package dev.pedro.rag.infra.llm.ollama.model.response
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
+import com.fasterxml.jackson.databind.annotation.JsonNaming
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
+data class OllamaChatStreamChunkResponse(
+    val model: String? = null,
+    val createdAt: String? = null,
+    val message: OllamaChatResponseMessage? = null,
+    val done: Boolean = false,
+    val response: String? = null,
+    val totalDuration: Long? = null,
+    val loadDuration: Long? = null,
+    val promptEvalCount: Int? = null,
+    val evalCount: Int? = null,
+)
