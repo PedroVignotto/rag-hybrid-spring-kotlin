1141d289b4f33e33cdd5a986ad8bde8d64a8cb0c
PedroVignotto <pedro.vignotto@hotmail.com>
2025-09-16 14:58:14 -0300

    test: ApiChatStreamMappers

---
+++ src/test/kotlin/dev/pedro/rag/api/chat/mappers/ApiChatStreamMappersTest.kt
@@ -0,0 +1,29 @@
+package dev.pedro.rag.api.chat.mappers
+
+import dev.pedro.rag.api.chat.response.stream.ApiChatUsageResponse
+import dev.pedro.rag.domain.chat.ChatUsage
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class ApiChatStreamMappersTest {
+
+    @Test
+    fun `should map ChatUsage domain to ApiChatUsageResponse`() {
+        val domain = ChatUsage(
+            promptTokens = 12,
+            completionTokens = 34,
+            totalDurationMs = 1234,
+            loadDurationMs = 567
+        )
+        val expected = ApiChatUsageResponse(
+            promptTokens = 12,
+            completionTokens = 34,
+            totalDurationMs = 1234,
+            loadDurationMs = 567
+        )
+
+        val actual = domain.toApiStreamResponse()
+
+        assertThat(actual).usingRecursiveComparison().isEqualTo(expected)
+    }
+}
