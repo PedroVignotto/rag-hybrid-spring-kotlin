d51ca212d67e8b88116bf60b22fdb85eb83d410d
PedroVignotto <pedro.vignotto@hotmail.com>
2025-09-24 14:17:35 -0300

    test: rename InMemoryVectorStoreTest to InMemoryVectorStoreContractTest

--- src/test/kotlin/dev/pedro/rag/infra/retrieval/vectorstore/memory/InMemoryVectorStoreContractTest.kt
+++ src/test/kotlin/dev/pedro/rag/infra/retrieval/vectorstore/memory/InMemoryVectorStoreContractTest.kt
@@ -4,7 +4,7 @@ import dev.pedro.rag.application.retrieval.ports.VectorStoreContractTest
 import dev.pedro.rag.application.retrieval.ports.VectorStorePort
 import dev.pedro.rag.domain.retrieval.CollectionSpec
 
-class InMemoryVectorStoreTest : VectorStoreContractTest() {
+class InMemoryVectorStoreContractTest : VectorStoreContractTest() {
     override fun sut(): VectorStorePort = InMemoryVectorStore()
 
     override fun collection(): CollectionSpec = CollectionSpec(provider = "test", model = "fake", dim = 3)
